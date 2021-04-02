f43d457d38de3a517fc7b0b993e77d2ff4bffcf1
bamobravo <bamobravo@gmail.com>
2021-04-02 11:12:41 +0100

    include the database table for the documents

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,6 +3,10 @@ import requests
 from bs4 import BeautifulSoup as bs
 import time
 import re
+import sys
+sys.path.append('classifier')
+import crawler_classifier as classifier
+import sqlite3
 
 class Crawler(threading.Thread):
 	"""docstring for Crawler"""
@@ -11,6 +15,7 @@ class Crawler(threading.Thread):
 		self.links = [site]
 		self.site_type=site_type
 		self.cache = cache
+		self.database_path = "./data/data.db"
 
 		#create a cache for check the page that has already been visited
 
@@ -33,22 +38,70 @@ class Crawler(threading.Thread):
 		if (not next_link) and self.isRelevantDataPage(content):
 			self.savePage(content)
 			return all_links
-		return all_links+next_link
+		return all_links.append(next_link)
 
 
 	def isRelevantDataPage(self,content):
 		#convert content to text first
 		# search for meta information and use the classifier module to determine how relavant the page is
-		text = content.get_text()
+		text = self.extractMainText(content)
+		metaPattern=r'\b(metadata|data\.json|publisher|licen(s|c)e|xls|csv|xlsx|(\w+ )+:$\b)'
+		total_matched = len(list(re.finditer(metaPattern,text,re.I|re.M)))
+		# check if the page has a download page
+		download_links = content.find_all('a',href=re.compile(r'\.*(download)\.*',flags=re.I|re.M))
+		if total_matched:
+			return total_matched > 10 and download_links and classifier.isGovernmentData(text)
+		return False
+	
+	def tablesExists(self):
+		table_check="SELECT name FROM sqlite_master WHERE type='table'"
+		cur = self.connection.cursor()
+		cur.execute(table_check)
+		result = cur.fetchone()
+		return result
 
-	def function(self,content):
-		text = content.get_text()
+	def createTables(self):
+		try:
+			if self.tablesExists():
+				return True
+			# document should one of the tables
+			table_creation="""
+				CREATE TABLE IF NOT EXISTS document (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL,metadata text,date_created TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP ,URL TEXT NOT NULL,content TEXT NOT NULL)
+			"""
+			cur = self.connection.cursor()
+			result = cur.execute(table_creation)
+			return True
+		except Exception as e:
+			return False
+
+	def savePage(self,content):
+		# get the the title of the document first
+		try:
+			if not self.connection:
+				self.connection = sqlite3.connect(self.database_path)
+		except Exception as e:
+			self.connection = sqlite3.connect(self.database_path)
+		if self.createTables():
+			heading=''
+			title_element = content.select('.module-content > h1,.column-two-thirds h1')
+			if title_element:
+				print(title_element)
+				heading = title_element[0].get_text()
+				print(heading)
+				exit()
+
+	def extractMainText(self,content):
+		body = content.select('main,article.prose')
+		if not body:
+			return ''
+		text = body[0].get_text()
+		return text
 		
 	def getNextLink(self,content):
 		temp = content.select('.pagination li:last-child a,.dgu-pagination__numbers > a')
 		if not temp:
 			return False
-		return self.temp.get('href')
+		return temp[0].get('href')
 
 	def wrapLink(self,current_link,link):
 		pattern =r'(https?://|www\.)[a-z0-9.\-_]+'
--- crawler/entry.py
+++ crawler/entry.py
@@ -2,7 +2,7 @@ from crawler import Crawler
 from cache import Cache
 
 #ckan.org is a data portal software
-sites =['https://data.gov','https://data.gov.uk','https://data.gov.uk/search?filters%5Btopic%5D=Business+and+economy','https://catalog.data.gov/dataset']
+sites =['https://catalog.data.gov/dataset/department-for-the-aging-dfta-geriatric-mental-health-contracted-providers','https://data.gov','https://data.gov.uk','https://data.gov.uk/search?filters%5Btopic%5D=Business+and+economy','https://catalog.data.gov/dataset']
 
 visitedData = Cache()
 def startCrawler():
