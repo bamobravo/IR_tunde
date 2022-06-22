fff76ee11715d17ce9f2b945ecbb3a0fa222faac
bamobravo <bamobravo@gmail.com>
2022-06-22 12:42:34 +0100

    built model for text classification with cosine similarity score

--- classifier/crawler_classifier.py
+++ classifier/crawler_classifier.py
@@ -4,6 +4,10 @@ import csv
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import os
+from sklearn.metrics.pairwise import cosine_similarity
 
 def getKeywords():
 	excludeWords = 'the is of for and new health'.split()
@@ -41,15 +45,16 @@ def convertToCSV(inputfile,outputfile):
 
 def splitTestTrain(filename):
 	data = read_csv(filename)
+	header = data.pop(0)
 	train, test = train_test_split(data,test_size=0.2,random_state=30)
 	name = filename.split('.',maxsplit=2)
 	name = name[0]
 	testname=name+'_test.csv'
 	trainname = name+'_train.csv'
 	train_pd = pd.DataFrame(train)
-	train_pd.to_csv(trainname)
+	train_pd.to_csv(trainname,header=header,index=False)
 	test_pd = pd.DataFrame(test)
-	test_pd.to_csv(testname)
+	test_pd.to_csv(testname,header=header,index=False)
 	print('data splitted')
 	exit()
 
@@ -61,9 +66,90 @@ def read_csv(filename):
 			result.append(list(row))
 	return result
 
+def buildVectors():
+	filePath = 'dmoz_links_train.csv'
+	all_data = pd.read_csv(filePath)
+	all_data['all_text']=all_data['title'].str.cat(all_data['description'],sep=' ')
+	categories= all_data['category'].unique()
+	for category in categories:
+		model,text = getVectors(all_data,category)
+		# now save the vector object for classification later
+		saveVector(model,category,text)
+
+def saveVector(model,category,text):
+	category = category.replace('/','_')
+	with open('models/'+category+'_model.bat','wb') as fl:
+		pickle.dump(model, fl)
+	with open('models/vectors/'+category+'_model.vec','wb') as fl:
+		temp = model.transform(text)
+		pickle.dump(temp,fl)
+	print(category,' saved')
+
+def getVectors(all_data,category):
+	data = all_data[all_data['category']==category]
+	doc = [x for x in data['all_text'].values.tolist() if x and isinstance(x,str) and x.strip()]
+	model = TfidfVectorizer()
+	result = model.fit(doc)
+	return result,doc
+
+def getScore(text):
+	folder='models/'
+	files = os.listdir(folder)
+	result=[]
+	for f in files:
+		path = folder+f
+		if os.path.isdir(path):
+			continue
+		with open(path,'rb') as fl:
+			item = pickle.load(fl)
+			vector = item.transform([text])
+			category = f.replace('_model.bat','')
+			# category = filename[0]
+			other = loadCategoryVector(category)
+			score = getSimilarityScore(vector,other)
+			result.append(score)
+	return max(result)
+
+def loadCategoryVector(category):
+	folder = 'models/vectors/'+category+"_model.vec"
+	with open(folder,'rb') as fl:
+		result = pickle.load(fl)
+		return result
+
+def getSimilarityScore(first, second):
+	result = cosine_similarity(first,second)
+	result = result[0]
+	return max(result)
+	# avg = sum(result)/len(result)
+	# return avg
+
+def classify(text):
+	'''
+		This will return true or false based on the threshold used
+	'''
+	threshold=0.5
+	if isinstance(text,str):
+		result = getScore(text)
+		return result >= threshold
+	result=[]
+	for t in text:
+		temp = getScore(t)
+		result.append(temp > threshold)
+	return result
+
+def getTestScores():
+	filePath = 'dmoz_links_test.csv'
+	all_data = pd.read_csv(filePath)
+	all_data['all_text']=all_data['title'].str.cat(all_data['description'],sep=' ')
+	data = all_data['all_text'].values.tolist()
+	data = data[:10]
+	result = classify(data)
+	return result
+
 # jsonFile ='dmoz_links.json'
-outputfile ='dmoz_links.csv'
-splitTestTrain(outputfile)
+# outputfile ='dmoz_links.csv'
+# splitTestTrain(outputfile)
 # convertToCSV(jsonFile,outputfile)	
 # data = pd.read_csv(outputfile)
 # data.heads()
+print(getTestScores())
